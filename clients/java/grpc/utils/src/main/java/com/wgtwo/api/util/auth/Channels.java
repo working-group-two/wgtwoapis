0d7c91994d174c29c7cf41188428cced02934c03
Gunnar Inge G. Sortland <gi@wgtwo.com>
2021-09-10 11:01:35 +0200

    [java] Update utils

--- clients/java/grpc/utils/src/main/java/com/wgtwo/api/util/auth/Channels.java
+++ clients/java/grpc/utils/src/main/java/com/wgtwo/api/util/auth/Channels.java
@@ -4,8 +4,12 @@ import com.wgtwo.api.common.Environment;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
-public class Clients {
+public class Channels {
     public static ManagedChannel createChannel(Environment environment) {
         return ManagedChannelBuilder.forAddress(environment.host, 443).build();
+    }
+
+    public static ManagedChannelBuilder<?> createChannelBuilder(Environment environment) {
+        return ManagedChannelBuilder.forAddress(environment.host, 443);
     }
 }
